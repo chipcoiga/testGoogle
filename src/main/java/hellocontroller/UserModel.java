f6fcb383698f4c2b68d66101f4ab1253a9e9b289
chipcoiga <lehuuloc.qt@gmail.com>
2016-07-11 16:16:39 +0700

    Create UserModel.java

---
+++ src/main/java/hellocontroller/UserModel.java
@@ -0,0 +1,202 @@
+
+package hellocontroller;
+
+public class UserModel {
+    private static final long serialVersionUID = 1L;
+
+    private int userID;
+
+    private String userName;
+
+    private String passWord;
+
+    private String emailAdd;
+
+    private int role;
+
+    private String phone;
+
+    private String address;
+
+    private String contactName;
+
+    private String country;
+
+    private String city;
+
+    private String message;
+
+    private boolean isActionSuccess;
+
+    private String status;
+
+    /**
+     * @return the userID
+     */
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @param userID the userID to set
+     */
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the passWord
+     */
+    public String getPassWord() {
+        return passWord;
+    }
+
+    /**
+     * @param passWord the passWord to set
+     */
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    /**
+     * @return the emailAdd
+     */
+    public String getEmailAdd() {
+        return emailAdd;
+    }
+
+    /**
+     * @param emailAdd the emailAdd to set
+     */
+    public void setEmailAdd(String emailAdd) {
+        this.emailAdd = emailAdd;
+    }
+
+    /**
+     * @return the role
+     */
+    public int getRole() {
+        return role;
+    }
+
+    /**
+     * @param role the role to set
+     */
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the contactName
+     */
+    public String getContactName() {
+        return contactName;
+    }
+
+    /**
+     * @param contactName the contactName to set
+     */
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    /**
+     * @return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public boolean isActionSuccess() {
+        return isActionSuccess;
+    }
+
+    public void setActionSuccess(boolean isActionSuccess) {
+        this.isActionSuccess = isActionSuccess;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
